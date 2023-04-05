e7f22d550f99bd7768104e28ca6530d69bb03577
Marco Schivo <marcoschivo@live.it>
2023-04-05 17:39:10 +0200

    cleaning

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,10 @@
-// #include "toojpeg.h"
-// #include <fstream>
 #include <iostream>
 #include "ppm.h"
-// std::ofstream render_file("output.jpg", std::ios_base::out | std::ios_base::binary);
-
-// void output_f(unsigned char byte) {
-//     render_file << byte;
-// } 
 
 int main() {
     
     const short width  = 800;
     const short height = 600;
-    // const int byte_per_pixel = 3;
-
-    // auto image = new unsigned char[width * height * byte_per_pixel];
 
     ppm map(width, height);
 
@@ -29,10 +19,6 @@ int main() {
     }
 
     map.write_file("output.ppm");
-    // const bool is_RGB = true;
-    // const unsigned char quality = 90;
-    // const bool downsample = false;
-    // TooJpeg::writeJpeg(output_f, image, width, height, is_RGB, quality, downsample, NULL);
 
     return 0;
 }
